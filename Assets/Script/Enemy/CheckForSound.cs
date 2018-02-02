40eb9c4e4f380ce808189fa95520bd069c5b5636
HoliestHoly <merlinkarmat@gmail.com>
2018-02-02 22:43:17 +0100

    Added enemy field of view

---
+++ Assets/Script/Enemy/CheckForSound.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckForSound : MonoBehaviour 
+{
+
+   
+
+    
+
+    void Update()
+	{
+		CheckSound (this.transform.position, 20);
+	}
+
+    void CheckSound(Vector3 center, float radius)
+	{
+        int layerId = 8;
+        int layerMask = 1 << layerId;
+        Collider[] soundInRadius = Physics.OverlapSphere (center, radius, layerMask);
+		int i = 0;
+		while (i < soundInRadius.Length) 
+		{
+			Debug.Log ("Sounds!");
+			i++;
+		}
+	}
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+     //Use the same vars you use to draw your Overlap SPhere to draw your Wire Sphere.
+     Gizmos.DrawWireSphere(this.transform.position, 20);
+    }
+
+
+
+}
---
+++ Assets/Script/Enemy/EnemyFieldOfView.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyFieldOfView : MonoBehaviour
+{
+    public float FieldOfViewAngle = 110f;
+    public bool playerInSight;
+    public Vector3 personalLastSighting;
+
+
+    private NavMeshAgent nav;
+    private SphereCollider col;
+    private GameObject player;
+
+    private void Awake()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        col = GetComponent<SphereCollider>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    private void Update()
+    {
+        Debug.Log(playerInSight);
+        
+    }
+   void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            playerInSight = false;
+
+            Vector3 direction = other.transform.position - transform.position;
+            float angle = Vector3.Angle(direction, transform.forward);
+
+            if(angle < FieldOfViewAngle * 0.5f)
+            {
+                RaycastHit hit;
+
+                if(Physics.Raycast(transform.position + transform.up, direction.normalized, out hit, col.radius))
+                {
+                    if(hit.collider.gameObject == player)
+                    {
+                        playerInSight = true;
+                        
+
+                    }
+                }
+            }
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+            playerInSight = false;
+    }
+
+
+}
